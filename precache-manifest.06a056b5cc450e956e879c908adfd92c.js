5eadd8fc1c1a8c210a021029dbc91767c77fac53
Pedro Goncalves <bpg.pedro@gmail.com>
2020-12-17 09:50:08 -0600

    Updates

---
+++ precache-manifest.06a056b5cc450e956e879c908adfd92c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4d329e3cad05e9e768c17008d55d41d",
+    "url": "/netflix-showcase-reactjs/index.html"
+  },
+  {
+    "revision": "55b092808d0c3b1f27c7",
+    "url": "/netflix-showcase-reactjs/static/css/main.16c28f0e.chunk.css"
+  },
+  {
+    "revision": "96c9a92dc91ca8843a12",
+    "url": "/netflix-showcase-reactjs/static/js/2.f1dfbf4b.chunk.js"
+  },
+  {
+    "revision": "c7e753f2e0694d3ab179c7e48eabb30d",
+    "url": "/netflix-showcase-reactjs/static/js/2.f1dfbf4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55b092808d0c3b1f27c7",
+    "url": "/netflix-showcase-reactjs/static/js/main.d6445d2c.chunk.js"
+  },
+  {
+    "revision": "b6449d13218f6baac071",
+    "url": "/netflix-showcase-reactjs/static/js/runtime-main.0787e7fa.js"
+  }
+]);
